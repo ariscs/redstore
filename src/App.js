d536258ef66b7cf6527353d08bd46d0db10d6e65
Aristides Camacho <acamachos11@outlook.com>
2020-05-08 18:38:45 -0500

    Links

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './default.scss';
 
-import Header from './components/Header';
+//Layout
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomepageLayout';
+
+//Pages
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <div className="main">
-                <Switch>
-                    <Route exact path="/" component={Homepage}/>
-                    <Route path="/registration" component={Registration}/>
-                </Switch>
-            </div>
+            <Switch>
+                <Route exact path="/" render={() => (
+                    <HomepageLayout>
+                        <Homepage />
+                    </HomepageLayout>
+                )} />
+                <Route path="/registration" render={() => (
+                    <MainLayout>
+                        <Registration />
+                    </MainLayout>
+                )} />
+            </Switch>
         </div>
     );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = props => {
+    return (
+        <footer className="footer">
+            <div className="wrap">
+                Proyecto Final Sistemas Integrados
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
