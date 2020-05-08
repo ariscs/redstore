499455fca70ed9987b6c16efd1c4b3d1b4f61f3b
Aristides Camacho <acamachos11@outlook.com>
2020-05-08 14:24:40 -0500

    Rutas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './default.scss';
 
 import Header from './components/Header';
 import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
 
 function App() {
     return (
         <div className="App">
             <Header />
             <div className="main">
-                <Homepage />
+                <Switch>
+                    <Route exact path="/" component={Homepage}/>
+                    <Route path="/registration" component={Registration}/>
+                </Switch>
             </div>
         </div>
     );
