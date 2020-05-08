b41175ab60f429ca6debe5d8af22b5602f3bb8d2
Aristides Camacho <acamachos11@outlook.com>
2020-05-08 13:59:45 -0500

    Categorias
    v1.0

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './styles.scss';
+
+import Board from './../../assets/board.jpg';
+import Sensor from './../../assets/sensor.jpg';
 
 const Directory = props => {
     return (
-        <div>
-
+        <div className="directory">
+            <div className="wrap">
+                <div className="category" style={{backgroundImage: `url(${Board})`}}>
+                    <a>Compra Tarjetas</a>
+                </div>
+                <div className="category" style={{backgroundImage: `url(${Sensor})`}}>
+                    <a>Compra Sensores</a>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './styles.scss';
 
 import Directory from './../../components/Directory'
 
 const Homepage = props => {
     return(
-        <section>
+        <section className="homepage">
             <Directory />
         </section>
     );
