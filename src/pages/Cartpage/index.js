f3a6939ac160940a10fda7dea96a49a083f2a05b
AIXAL <aixa.lievana@gmail.com>
2020-06-02 13:00:16 -0500

    estilos carrito

--- src/pages/Cartpage/index.js
+++ src/pages/Cartpage/index.js
@@ -4,26 +4,30 @@ import './styles.scss';
 const Cartpage = props => {
     return(
         <section className="homepage">
-            <div className="wrap">
-                <div className="cart">
-                    <div className="title">
-                        <h1>Carrito</h1>
-                    </div>
+            <div className="shop">
+            <div className="cart">
+                 <div className="title">
+                        <h3>Carrito de compras</h3>
+                </div>
                     <div className="items">
                         
                     </div>
-                    <div className="total">
+                <div className="back">Continuar comprando</div>
+            </div>
+            <div className="wrap2">
+                    <h3>Detalles de compra</h3>
+                    <div className="form">
+                        <input type="text" placeholder="Estado" />
+                        <input type="text" placeholder="Ciudad" />
+                        <input type="text" placeholder="Dirección" />
+                    </div>
+                      <div className="total">
                         <div className="total--text">
                             <p>Total:</p>
                         </div>
                         <div className="total--price">
                             <p>$.00</p>
                         </div>    
-                    </div>
-                    <div className="form">
-                        <input type="text" placeholder="Estado" />
-                        <input type="text" placeholder="Ciudad" />
-                        <input type="text" placeholder="Dirección" />
                     </div>
                     <div className="buy">
                         <button>Comprar</button>
